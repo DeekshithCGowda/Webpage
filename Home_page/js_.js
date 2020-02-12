f388ff68bc35f9bc972db9d5da8478bea5f834fd
DeekshithCGowda <60966249+DeekshithCGowda@users.noreply.github.com>
2020-02-13 02:07:06 +0530

    update without time

--- Home_page/js_.js
+++ Home_page/js_.js
@@ -47,58 +47,6 @@ var showCurrentTime = function()
     clock.innerText = clockTime;
 };
 
-// Getting the clock to increment on its own and change out messages and pictures
-var updateClock = function() 
-{
-  var time = new Date().getHours();
-  var messageText;
-  var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-
-  var timeEventJS = document.getElementById("timeEvent");
-  var lolcatImageJS = document.getElementById('lolcatImage');
-  
-  if (time == partytime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
-    messageText = "Get to work  ";
-  }
-  else if (time == wakeuptime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
-    messageText = "Wake up!";
-  }
-  else if (time == lunchtime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
-    messageText = "Study hard!";
-  }
-  else if (time == naptime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
-    messageText = "Sweat hard!";
-  }
-  else if (time < noon)
-  {
-    image = "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg";
-    messageText = "Good morning!";
-  }
-  else if (time >= evening)
-  {
-    image = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cat_sleep.jpg";
-    messageText = "Good evening!";
-  }
-  else
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-    messageText = "Good afternoon!";
-  }
-
-  console.log(messageText); 
-  timeEventJS.innerText = messageText;
-  lolcatImage.src = image;
-  
-  showCurrentTime();
-};
 updateClock();
 
 // Getting the clock to increment once a second
